47b2531e67e9f6da11a3d3913fa544f643e0dbea
LeeInTaek <hotdog2275323@gmail.com>
2020-10-21 12:44:55 +0900

    Apply admin-lte

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/modules/adminlte',express.static(path.join(__dirname, 'node_modules/admin-lte')));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Test' });
 });
-
+router.get('/boardPage', function(req, res, next) {
+  res.render('pages/boardPage', { title: 'Test' });
+});
 module.exports = router;
